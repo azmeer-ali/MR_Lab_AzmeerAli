a4459764c2544490e597bfb58ba0a16ed2f6ba2e
azmeer-ali <azmeerali1003@gmail.com>
2026-03-09 07:28:35 +0000

    Mission Accomplished

---
+++ week_1/my_first_pkg/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_first_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='azmeer53',
+    maintainer_email='azmeer53@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': ['simple_node = my_first_pkg.simple_node:main','task_2 = my_first_pkg.task_2:main','task_1 = my_first_pkg.task_1:main',
+	 ],
+    },
+)
